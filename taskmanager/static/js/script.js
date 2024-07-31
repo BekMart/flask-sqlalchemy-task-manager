bb17bd1dec75343c01f423b1753fc4b218fc9017
Rebecca Martin <becki_213@hotmail.com>
2024-07-31 08:12:16 +0000

    create add task function and form to enable user to add tasks and link them to categories

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialisation
-    var sidenav = document.querySelectorAll('.sidenav');
+    let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    //datepicker initialisation
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy", // shows date as 01 Janurary, 2024
+      i18n: {done: "Select"} // changes text on done button to select
+    });
+
+    // select initialisation
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
